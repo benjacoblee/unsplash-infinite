17a88801e078d12b400142e76230b6b005a07df4
benn <benjacoblee@gmail.com>
2020-08-25 11:00:55 +0800

    fetches photos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
-    return <div className="App">Unsplash</div>;
-}
+const App = () => {
+    const [photos, setPhotos] = useState([]);
+    const fetchPhotos = async () => {
+        const response = await axios.get("https://api.unsplash.com/photos", {
+            headers: {
+                Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+            }
+        });
+
+        setPhotos(response.data);
+    };
+    const renderPhotos = () => {
+        return photos.map((photo) => {
+            const { id, description, alt_description, urls } = photo;
+            return (
+                <div>
+                    <img src={urls.regular} alt={alt_description} />
+                    <p> {description || "No caption provided"}</p>
+                </div>
+            );
+        });
+    };
+    useEffect(() => {
+        fetchPhotos();
+    }, []);
+    return (
+        <div className="App">
+            <div className="container">{renderPhotos()}</div>
+        </div>
+    );
+};
 
 export default App;
