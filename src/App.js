861baf38e4a1543d2d46487a5f1cb320bf0828ef
benn <benjacoblee@gmail.com>
2020-08-25 15:27:35 +0800

    implemented infinite scroll, basic styling

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Photos from "./Photos";
+import "./App.css";
 
 const App = () => {
+    const [page, setPage] = useState(1);
     const [photos, setPhotos] = useState([]);
-    const fetchPhotos = async () => {
-        const response = await axios.get("https://api.unsplash.com/photos", {
-            headers: {
-                Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
-            }
-        });
+    const [isFetching, setIsFetching] = useState(false);
 
-        setPhotos(response.data);
+    const fetchPhotos = async () => {
+        setPage((page) => page + 1);
+        const response = await axios.get(
+            `https://api.unsplash.com/photos?page=${page}&per_page=10&order_by=popular`,
+            {
+                headers: {
+                    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+                }
+            }
+        );
+
+        setPhotos([...photos, ...response.data]);
     };
-    const renderPhotos = () => {
-        return photos.map((photo) => {
-            const { id, description, alt_description, urls } = photo;
-            return (
-                <div>
-                    <img src={urls.regular} alt={alt_description} />
-                    <p> {description || "No caption provided"}</p>
-                </div>
-            );
-        });
-    };
+
     useEffect(() => {
         fetchPhotos();
     }, []);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    useEffect(() => {
+        if (!isFetching) return;
+        fetchPhotos();
+        setIsFetching(false);
+    });
+
+    function handleScroll() {
+        if (
+            window.innerHeight + document.documentElement.scrollTop !==
+            document.documentElement.offsetHeight
+        )
+            return;
+        else {
+            setIsFetching(true);
+        }
+    }
+
     return (
         <div className="App">
-            <div className="container">{renderPhotos()}</div>
+            <div className="container">
+                <Photos photos={photos} />
+            </div>
         </div>
     );
 };
---
+++ src/Photos.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useEffect } from "react";
+import "./App.css";
+
+const Photos = ({ photos }) => {
+    if (photos.length) {
+        return photos.map((photo) => {
+            const { id, description, alt_description, urls } = photo;
+            return (
+                <div key={id}>
+                    <img src={urls.regular} alt={alt_description} />
+                    <p> {description || null}</p>
+                </div>
+            );
+        });
+    }
+
+    return null;
+};
+
+export default Photos;
